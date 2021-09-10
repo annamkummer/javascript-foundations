8125475253c448c7b90d22c815eeb69201a378c4
Anna Kummer <annamkummer@gmail.com>
2021-09-10 15:28:38 -0600

    Complete crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,27 @@
-class Craft {
+var Material = require('./material');
 
+class Craft {
+  constructor(craft) {
+    this.name = craft.type;
+    this.materials = craft.materials;
+    this.completed = false;
+    this.totalCost = 0;
+  }
+
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!'
+  }
+
+  calculateProjectTotal() {
+    for (var i = 0; i < this.materials.length; i++) {
+      var material = this.materials[i];
+      var newMaterial = new Material(material.name, material.price, material.amount, material.units)
+      var cost = newMaterial.calculateMaterialCost()
+      this.totalCost += cost;
+    }
+    return this.totalCost;
+  }
 }
 
 module.exports = Craft;
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,23 @@
 class Material {
+  constructor(name, price, amount, units) {
+    this.name = name;
+    this.price  = price;
+    this.amount = amount;
+    this.units = units;
+  }
 
+  useMaterial(fabricUsed) {
+    if (fabricUsed > this.amount) {
+      return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+    } else {
+      this.amount -= fabricUsed;
+      return `You now have ${this.amount} ${this.units} of ${this.name} left.`;
+    }
+  }
+
+  calculateMaterialCost() {
+    return this.price * this.amount;
+  }
 }
 
 module.exports = Material;
